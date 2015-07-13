51a176cf68208910fcc9ac9c64009495ed8764f2
Curtis McEnroe <programble@gmail.com>
2015-07-13 13:22:26 -0400

    Fix primary keys once again

--- sql/14.create-table-synset-synsets.sql
+++ sql/14.create-table-synset-synsets.sql
@@ -1,7 +1,7 @@
 CREATE TABLE synset_synsets (
   source_synset_id text NOT NULL REFERENCES synsets,
+  pointer_symbol pointer_symbol NOT NULL,
   target_synset_id text NOT NULL,
   target_pos pos NOT NULL,
-  pointer_symbol pointer_symbol NOT NULL,
-  PRIMARY KEY (source_synset_id, target_synset_id)
+  PRIMARY KEY (source_synset_id, pointer_symbol, target_synset_id)
 );
