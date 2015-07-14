37616dff2f07042ba2962095e624fed66b8f2448
Curtis McEnroe <programble@gmail.com>
2015-07-13 23:14:09 -0400

    Implement data parsing

--- Makefile
+++ Makefile
@@ -1,21 +1,32 @@
-DATABASE = wdpg
 RUBY = ruby
+PSQL = psql
+
+PSQL_FLAGS = -X -v ON_ERROR_STOP=true -e
+
 DICT_TAR = wn3.1.dict.tar.gz
+DATABASE = wdpg
 
 PRE = $(wildcard sql/0?.*.sql) $(wildcard sql/1?.*.sql)
 DYNAMIC = sql/20.insert-lemmas-noun.sql
 DYNAMIC += sql/20.insert-lemmas-verb.sql
 DYNAMIC += sql/20.insert-lemmas-adj.sql
 DYNAMIC += sql/20.insert-lemmas-adv.sql
+DYNAMIC += sql/21.insert-synsets-noun.sql
+DYNAMIC += sql/21.insert-synsets-verb.sql
+DYNAMIC += sql/21.insert-synsets-adj.sql
+DYNAMIC += sql/21.insert-synsets-adv.sql
 POST = $(wildcard sql/3?.*.sql)
 
 all: $(PRE) $(DYNAMIC) $(POST)
 	set -e; for source in $^; do \
-	  psql -X -v ON_ERROR_STOP=true -e -f $$source $(DATABASE); \
+	  $(PSQL) $(PSQL_FLAGS) -f $$source $(DATABASE); \
 	done
 
 sql/20.insert-lemmas-%.sql: dict/index.%
 	$(RUBY) parsers/parse-index.rb $< > $@
+
+sql/21.insert-synsets-%.sql: dict/data.%
+	$(RUBY) parsers/parse-data.rb $< > $@
 
 dict/%: dict
 	
---
+++ parsers/parse-data.rb
@@ -0,0 +1,105 @@
+#!/usr/bin/env ruby
+
+ARGF.each_line do |line|
+  next if line.start_with? '  '
+  split = line.split ' | '
+  columns = split[0].split ' '
+  gloss = split[1].strip.gsub /'/, "''"
+
+  synset_offset = columns.shift
+  lex_filenum = columns.shift.to_i
+  pos = columns.shift
+  word_count = columns.shift.to_i 16
+
+  words = []
+  word_count.times do
+    word = columns.shift
+    word.gsub! /_/, ' '
+    word.gsub! /'/, "''"
+    lex_id = columns.shift.to_i 16
+    syntactic_marker = 'NULL'
+
+    word.match /^([^(]+)(\(([a-z]+)\))?$/ do |m|
+      if m[3]
+        word = m[1]
+        syntactic_marker = "'#{m[3]}'"
+      end
+    end
+
+    words << [word, syntactic_marker, lex_id]
+  end
+
+  pointer_count = columns.shift.to_i
+  pointers = []
+  pointer_count.times do
+    pointer_symbol = columns.shift.gsub /\\/, '\\\\'
+    target_offset = columns.shift
+    target_pos = columns.shift
+
+    wst = columns.shift
+    source_word = wst[0, 2].to_i 16
+    target_word = wst[2, 2].to_i 16
+
+    pointers << [pointer_symbol, target_offset, target_pos, source_word, target_word]
+  end
+  synset_pointers, word_pointers = pointers.uniq.partition {|p| p[3] == 0 }
+
+  # TODO: Frames
+
+  puts <<-EOF
+WITH synset AS (
+  INSERT INTO synsets
+  (id, lex_filenum, pos, word_count, pointer_count, glossary)
+  VALUES (
+    '#{pos}#{synset_offset}', #{lex_filenum}, '#{pos}', #{word_count},
+    #{pointer_count}, '#{gloss}'
+  )
+  RETURNING id
+), synset_words AS (
+  INSERT INTO words (synset_id, word, syntactic_marker, lex_id, word_number)
+  VALUES
+  EOF
+  values = words.map.with_index do |w, i|
+    "('#{pos}#{synset_offset}', '#{w[0]}', #{w[1]}, #{w[2]}, #{i+1})"
+  end.join ', '
+  puts '    ' + values
+  puts <<-EOF
+  RETURNING id, word_number
+)
+  EOF
+  if synset_pointers.any?
+    puts <<-EOF
+, synset_pointers AS (
+  INSERT INTO synset_synsets
+  (source_synset_id, target_synset_id, target_pos, pointer_symbol)
+  VALUES
+    EOF
+    values = synset_pointers.map.with_index do |p, i|
+      "('#{pos}#{synset_offset}', '#{p[2]}#{p[1]}', '#{p[2]}', '#{p[0]}')"
+    end.join ', '
+    puts '    ' + values
+    puts ")"
+  end
+  if word_pointers.any?
+    puts <<-EOF
+, word_pointers_raw (ps, tsi, tpos, swn, twn) AS (
+  VALUES
+    EOF
+    values = word_pointers.map do |ps, to, tpos, swn, twn|
+      "('#{ps}', '#{tpos}#{to}', '#{tpos}', #{swn}, #{twn})"
+    end.join ', '
+    puts '    ' + values
+    puts <<-EOF
+)
+INSERT INTO word_words
+(source_word_id, target_synset_id, target_pos, target_word_number, pointer_symbol)
+SELECT synset_words.id, w.tsi, w.tpos::pos, w.twn, w.ps::pointer_symbol
+FROM word_pointers_raw w, synset_words
+WHERE synset_words.word_number = w.swn;
+    EOF
+  else
+    puts "SELECT NULL;"
+  end
+
+  puts
+end
